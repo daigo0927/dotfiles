8ec89a983325f24897a3c39f258104b33e29d5c5
Daigo Hirooka <daigo.hirooka@gmail.com>
2020-05-23 06:09:24 +0900

    enable emacs to watch shell environment

--- zsh/emacs.zsh
+++ zsh/emacs.zsh
@@ -2,3 +2,8 @@ if [ "$(uname)" = "Darwin" ]; then
    alias emacs="emacs"
 else # Linux
 fi
+
+## create emacs env file
+perl -wle \
+    'do { print qq/(setenv "$_" "$ENV{$_}")/ if exists $ENV{$_} } for @ARGV' \
+    PATH > ~/.emacs.d/shellenv.el
