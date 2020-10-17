7ec61f25439ad310c5be1e078e517f4229c627f3
Daigo Hirooka <daigo.hirooka@gmail.com>
2020-10-18 05:12:27 +0900

    fix melpa URL: melpa.milkbox.net -> melpa.org

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -12,7 +12,7 @@
 
 ;; package management
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (fset 'package-desc-vers 'package--ac-desc-version)
 (package-initialize)
