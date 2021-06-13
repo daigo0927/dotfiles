c01e24a667900196a2cc2b701414bdb01a73d3f8
Daigo Hirooka <daigo.hirooka@gmail.com>
2021-06-14 00:12:14 +0900

    update pyenv init statement

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,7 +1,7 @@
 export PYENV_ROOT="$HOME/.pyenv"
 if [ -d ${PYENV_ROOT} ]; then
   export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init -)"
+  eval "$(pyenv init --path)"
   if [ -d ${PYENV_ROOT}/plugins/pyenv-virtualenv ]; then
     eval "$(pyenv virtualenv-init -)"
   fi
