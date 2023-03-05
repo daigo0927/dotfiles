f71dd2fc43b442cd56156ceb1febc2d0338135f0
Daigo Hirooka <daigo.hirooka@gmail.com>
2023-03-05 18:48:31 +0900

    resolve revert (doom-theme commit)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -160,29 +160,34 @@
     (setq imenu-create-index-function 'python-imenu-create-index)))
 
 ;; color theme
-(use-package doom-themes
+(use-package rebecca-theme
   :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t  ; if nil, italics is universally disabled
-	)
-  (load-theme 'doom-one t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config)
-
-  (custom-set-faces
-   `(mode-line ((t (:background ,(doom-color 'dark-violet)))))
-   `(font-lock-comment-face ((t (:foreground ,(doom-color 'base7))))))
+  :config (load-theme 'rebecca t)
   )
+
+;; (use-package doom-themes
+;;   :ensure t
+;;   :config
+;;   ;; Global settings (defaults)
+;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;         doom-themes-enable-italic t  ; if nil, italics is universally disabled
+;; 	)
+;;   (load-theme 'doom-one t)
+
+;;   ;; Enable flashing mode-line on errors
+;;   (doom-themes-visual-bell-config)
+;;   ;; Enable custom neotree theme (all-the-icons must be installed!)
+;;   (doom-themes-neotree-config)
+;;   ;; or for treemacs users
+;;   (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+;;   (doom-themes-treemacs-config)
+;;   ;; Corrects (and improves) org-mode's native fontification.
+;;   (doom-themes-org-config)
+
+;;   (custom-set-faces
+;;    `(mode-line ((t (:background ,(doom-color 'dark-violet)))))
+;;    `(font-lock-comment-face ((t (:foreground ,(doom-color 'base7))))))
+;;   )
 
 ;; markdown preview
 (autoload 'markdown-preview-mode "markdown-preview-mode.el" t)
