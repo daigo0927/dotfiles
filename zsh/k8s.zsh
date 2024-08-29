d1a75e1b63bb6b0b146cefb45f820f5f430550b0
Daigo Hirooka <daigo.hirooka@gmail.com>
2024-08-30 04:20:12 +0900

    update k8s settings

--- zsh/k8s.zsh
+++ zsh/k8s.zsh
@@ -3,3 +3,5 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 [[ $commands[kubectl] ]] && source <(kubectl completion zsh)
 
 alias k="kubectl"
+source "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh"
+PS1='$(kube_ps1)'$PS1
